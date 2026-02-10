14e5ccc12271b9ab6b32f061b4b67a3d90eaccc0
Yuval Gabay <36155483+GuitaristForEver@users.noreply.github.com>
2026-02-10 09:33:27 +0200

    Fix formatting of docstring in power method

--- src/calculator.py
+++ src/calculator.py
@@ -27,7 +27,7 @@ class Calculator:
         return a / b
 
     def power(self, a: float, b: float) -> float:
-        """Raise a to the power of b. """
+        """Raise a to the power of b."""
         # TODO: add tests
         return a**b
 
